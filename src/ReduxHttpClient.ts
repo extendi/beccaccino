3e0d68d8b94235e87e07bf4a97dc5bd1be99cacb
Carmine Di Monaco <dimonaco@extendi.it>
2018-07-18 18:09:32 +0200

    [skip ci] wip

--- src/ReduxHttpClient.ts
+++ src/ReduxHttpClient.ts
@@ -6,22 +6,31 @@ export default class ReduxHttpClient {
 
   private readonly axiosInstance : AxiosInstance;
   private readonly axiosConfiguration : AxiosRequestConfig;
+  private readonly endpoints : any; // We should implement the type
 
-  private constructor(axiosConfiguration: AxiosRequestConfig) {
+  private constructor(axiosConfiguration: AxiosRequestConfig, endpoints : any) {
     this.axiosConfiguration = axiosConfiguration;
     this.axiosInstance = axios.create(this.axiosConfiguration);
+    this.endpoints = endpoints;
   }
 
   static setupHttpClient(
     axiosConfiguration : AxiosRequestConfig,
+    endpoints : any,
   ) { // TODO: When axios works add here the endpoints? @sciamp
-    if (!this.clientInstance) this.clientInstance = new ReduxHttpClient(axiosConfiguration);
+    if (!this.clientInstance) {
+      this.clientInstance = new ReduxHttpClient(axiosConfiguration, endpoints);
+    }
     return this.clientInstance;
   }
 
   static getClient() {
     invariant(!this.clientInstance, 'redux-http-client not configured yet!');
     return this.clientInstance;
+  }
+
+  getEndpoints() {
+    return this.endpoints; // Later the binded endpoints
   }
 
   getAxiosIntance() {
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { default as ReduxHttpClient } from './configureClient';
