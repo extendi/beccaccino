b39dacade06bf022bb6805d7f0d79eb582e98af3
Carmine Di Monaco <dimonaco@extendi.it>
2018-07-19 16:37:16 +0200

    Add request details to the redux action dispatched by client [skip ci]

--- src/endpoint/Endpoint.ts
+++ src/endpoint/Endpoint.ts
@@ -5,13 +5,18 @@ const bindParamsToURL = (url : string, params: any) => url; // TODO DO THE REAL 
 
 export default class Endpoint {
   static bindAction(bindRequest: BindRequest): BindedAction {
-    return (params : any) => ({
+    return ({ urlParams = {}, requestPayload = {} } : { urlParams: any, requestPayload: any}) => ({
       type: bindRequest.actioName,
       signature: bindRequest.signature,
+      requestDetails: {
+        urlParams,
+        requestPayload,
+      },
       execAsync: requestHandler({
         requestConfiguration: {
           method: bindRequest.config.method,
-          url: bindParamsToURL(bindRequest.config.path, params),
+          url: bindParamsToURL(bindRequest.config.path, urlParams),
+          data: requestPayload,
         },
         errorTransformer: bindRequest.config.errorTransformer,
         responseTransformer: bindRequest.config.responseTransformer,
--- src/redux-http/index.ts
+++ src/redux-http/index.ts
@@ -1,11 +1,17 @@
-import { AxiosResponse } from "axios";
+import { AxiosResponse } from 'axios';
 
 export const REDUX_HTTP_CLIENT_REQUEST = '@@redux_http_client/request';
+export const REDUX_HTTP_CLIENT_ERROR = '@@redux_http_client/error';
+export const REDUX_HTTP_CLIENT_RESPONSE = '@@redux_http_client/response';
 
 export const REDUX_HTTP_ACTION_SIGNATURE = Symbol('REDUX:HTTP_CLIENT_ACTION');
 
 export type BindedActionPayload = {
   type: string,
   signature: Symbol,
+  requestDetails: {
+    urlParams: any,
+    requestPayload : any,
+  }
   execAsync: Promise<AxiosResponse>,
 };
