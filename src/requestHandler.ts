7fbf8b3a3f0ede3bcde2363ec9d3f451f708770e
Carmine Di Monaco <dimonaco@extendi.it>
2018-07-19 12:39:49 +0200

    Added error and response transformers as parameter of requestHandler [skip ci]

--- src/requestHandler.ts
+++ src/requestHandler.ts
@@ -1,22 +1,17 @@
 import ReduxHttpClient from 'ReduxHttpClient';
-import { AxiosResponse, AxiosRequestConfig, AxiosInstance } from '../node_modules/axios';
-
-// I used the default parameter for 'dependency injection' and testing do you agree?
-// When we implement the endpoints, every enpoint can have
-// his transform function and we can access them
-// using a method in the class and injecting it in this function in the then of request
-// you agree? @sciamp
-
-// For dev purpose
+import { AxiosResponse, AxiosRequestConfig, AxiosInstance } from 'axios';
+import { ErrorTransform, ResponseTransform } from 'Endpoint';
 
 const defaulTransformFunction = (x : any) : any => x;
 const defaultErrorTransformFunction = (x : any) : any => x;
 
 export default function requestHandler(
   requestConfiguration: AxiosRequestConfig,
+  errorTransformer: ErrorTransform = defaultErrorTransformFunction,
+  responseTransformer: ResponseTransform = defaulTransformFunction,
   axiosInstance : AxiosInstance = ReduxHttpClient.getClient().getAxiosIntance(),
 ) : Promise<AxiosResponse> {
   return axiosInstance.request(requestConfiguration)
-  .then(response => defaulTransformFunction(response))
-  .catch(error => defaultErrorTransformFunction(error));
+  .then(response => responseTransformer(response))
+  .catch(error => errorTransformer(error));
 }
