091103d2b4e083f92fb751ca943d2f9fecae3f1a
Carmine Di Monaco <dimonaco@extendi.it>
2018-07-19 12:41:43 +0200

    example http client 😉😗😛😝😇😗

--- src/ReduxHttpClient.ts
+++ src/ReduxHttpClient.ts
@@ -1,12 +1,13 @@
 import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
 import { Endpoints } from 'Endpoint';
+import requestHandler from 'requestHandler';
 
 class ReduxHttpClient {
   private readonly axiosInstance : AxiosInstance;
   private readonly axiosConfiguration : AxiosRequestConfig;
   private readonly endpoints : Endpoints; // We should implement the type
 
-  constructor(axiosConfiguration: AxiosRequestConfig, endpoints : any) {
+  constructor(axiosConfiguration: AxiosRequestConfig, endpoints : Endpoints) {
     this.axiosConfiguration = axiosConfiguration;
     this.axiosInstance = axios.create(this.axiosConfiguration);
     this.endpoints = endpoints;
@@ -16,8 +17,10 @@ class ReduxHttpClient {
   private bindEndpoints() : void {
     this.endpoints.forEach((endpoint) => {
       const { name, path } = endpoint;
-      this[name] = () => {
+      this[name] = async () => {
         console.log(`I am the method with ${name} and ${path}`);
+        const response = await requestHandler({ url: path, method: 'get' });
+        console.log('The response is ', response);
       };
     });
   }
@@ -32,7 +35,7 @@ class ReduxHttpClient {
 const reduxHttpClientInitializer = (() => {
   let clientInstance : ReduxHttpClient = null;
   return  {
-    configure: (configuration : AxiosRequestConfig , endpoints : any) :ReduxHttpClient => {
+    configure: (configuration : AxiosRequestConfig , endpoints : Endpoints) :ReduxHttpClient => {
       if (clientInstance) throw Error('Redux http client instance already configured');
       clientInstance = new ReduxHttpClient(configuration, endpoints);
       return clientInstance;
