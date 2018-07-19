5f6dad5c9741f9b9ca1996b55fac73c7ab9eb3fc
Carmine Di Monaco <dimonaco@extendi.it>
2018-07-19 17:31:07 +0200

    Extract requestDetails into an external type, added BindedActionResultPayload as the type of action returned by middleware when a redux http client action is detected

--- src/redux-http/index.ts
+++ src/redux-http/index.ts
@@ -1,4 +1,4 @@
-import { AxiosResponse } from 'axios';
+import { EndpointResponse } from 'endpoint';
 
 export const REDUX_HTTP_CLIENT_REQUEST = '@@redux_http_client/request';
 export const REDUX_HTTP_CLIENT_ERROR = '@@redux_http_client/error';
@@ -6,12 +6,21 @@ export const REDUX_HTTP_CLIENT_RESPONSE = '@@redux_http_client/response';
 
 export const REDUX_HTTP_ACTION_SIGNATURE = Symbol('REDUX:HTTP_CLIENT_ACTION');
 
+export type BindedRequestDetails = {
+  urlParams: any,
+  requestPayload : any,
+  endpointName: string,
+};
+
 export type BindedActionPayload = {
   type: string,
   signature: Symbol,
-  requestDetails: {
-    urlParams: any,
-    requestPayload : any,
-  }
-  execAsync: Promise<AxiosResponse>,
+  requestDetails: BindedRequestDetails,
+  execAsync: Promise<EndpointResponse>,
+};
+
+export type BindedActionResultPayload = {
+  type: string,
+  requestDetails: BindedRequestDetails,
+  response: EndpointResponse,
 };
