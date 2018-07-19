df560bcc8841f2406f781dec99715c1ea96ec408
Carmine Di Monaco <dimonaco@extendi.it>
2018-07-19 11:07:27 +0200

    [skip ci]wip client

--- src/ReduxHttpClient.ts
+++ src/ReduxHttpClient.ts
@@ -1,39 +1,47 @@
 import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
-import invariant from 'invariant';
+import { Endpoints } from 'Endpoint';
 
-export default class ReduxHttpClient {
-  public static clientInstance : ReduxHttpClient;
-
+class ReduxHttpClient {
   private readonly axiosInstance : AxiosInstance;
   private readonly axiosConfiguration : AxiosRequestConfig;
-  private readonly endpoints : any; // We should implement the type
+  private readonly endpoints : Endpoints; // We should implement the type
 
-  private constructor(axiosConfiguration: AxiosRequestConfig, endpoints : any) {
+  constructor(axiosConfiguration: AxiosRequestConfig, endpoints : any) {
     this.axiosConfiguration = axiosConfiguration;
     this.axiosInstance = axios.create(this.axiosConfiguration);
     this.endpoints = endpoints;
+    this.bindEndpoints();
   }
 
-  static setupHttpClient(
-    axiosConfiguration : AxiosRequestConfig,
-    endpoints : any,
-  ) { // TODO: When axios works add here the endpoints? @sciamp
-    if (!this.clientInstance) {
-      this.clientInstance = new ReduxHttpClient(axiosConfiguration, endpoints);
-    }
-    return this.clientInstance;
+  private bindEndpoints() : void {
+    this.endpoints.forEach((endpoint) => {
+      const { name, path } = endpoint;
+      this[name] = () => {
+        console.log(`I am the method with ${name} and ${path}`);
+      };
+    });
   }
-
-  static getClient() {
-    invariant(!this.clientInstance, 'redux-http-client not configured yet!');
-    return this.clientInstance;
-  }
-
   getEndpoints() {
-    return this.endpoints; // Later the binded endpoints
+    return this.endpoints;
   }
-
   getAxiosIntance() {
     return this.axiosInstance;
   }
 }
+
+const reduxHttpClientInitializer = (() => {
+  let clientInstance : ReduxHttpClient = null;
+  return  {
+    configure: (configuration : AxiosRequestConfig , endpoints : any) :ReduxHttpClient => {
+      if (clientInstance) throw Error('Redux http client instance already configured');
+      clientInstance = new ReduxHttpClient(configuration, endpoints);
+      return clientInstance;
+    },
+    getClient: () :ReduxHttpClient => {
+      if (!clientInstance) throw Error('Redux http client instance not configured');
+      return clientInstance;
+    },
+  };
+})();
+
+export default reduxHttpClientInitializer;
