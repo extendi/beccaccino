dc8e7727a47b48372f68056035c789ec79f966ff
sciamp <alessandro.campagni@gmail.com>
2018-07-20 18:41:37 +0200

    Fix indentation in ReduxHttpClient

--- src/ReduxHttpClient.ts
+++ src/ReduxHttpClient.ts
@@ -1,25 +1,25 @@
-import axios, { AxiosInstance, AxiosRequestConfig }from 'axios';
-import { EndpointConfig, Endpoint, BindedAction }from '@lib/endpoint';
-import { REDUX_HTTP_ACTION_SIGNATURE }from '@lib/redux-http';
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
+import { EndpointConfig, Endpoint, BindedAction } from '@lib/endpoint';
+import { REDUX_HTTP_ACTION_SIGNATURE } from '@lib/redux-http';
 
 export type EndpointMap = {
-  [key : string]: BindedAction,
+  [key: string]: BindedAction,
 };
 
 class ReduxHttpClient {
-  private readonly axiosInstance : AxiosInstance;
-  private readonly axiosConfiguration : AxiosRequestConfig;
-  private readonly endpoints : Array<EndpointConfig>;
-  public readonly bindedEndpoints : EndpointMap = {};
+  private readonly axiosInstance: AxiosInstance;
+  private readonly axiosConfiguration: AxiosRequestConfig;
+  private readonly endpoints: Array<EndpointConfig>;
+  public readonly bindedEndpoints: EndpointMap = {};
 
-  constructor(axiosConfiguration: AxiosRequestConfig, endpoints : Array<EndpointConfig>) {
+  constructor(axiosConfiguration: AxiosRequestConfig, endpoints: Array<EndpointConfig>) {
     this.axiosConfiguration = axiosConfiguration;
     this.axiosInstance = axios.create(this.axiosConfiguration);
     this.endpoints = endpoints;
     this.bindEndpoints();
   }
 
-  private bindEndpoints() : void {
+  private bindEndpoints(): void {
     this.endpoints.forEach((endpoint) => {
       this.bindedEndpoints[endpoint.name] = Endpoint.bindAction({
         config: endpoint,
@@ -32,17 +32,17 @@ class ReduxHttpClient {
 }
 
 const reduxHttpClientInitializer = (() => {
-  let clientInstance : ReduxHttpClient = null;
-  return  {
+  let clientInstance: ReduxHttpClient = null;
+  return {
     configure: (
-      configuration : AxiosRequestConfig ,
-      endpoints : Array<EndpointConfig>,
-      ) :EndpointMap => {
+      configuration: AxiosRequestConfig,
+      endpoints: Array<EndpointConfig>,
+    ): EndpointMap => {
       if (clientInstance) throw Error('Redux http client instance already configured');
       clientInstance = new ReduxHttpClient(configuration, endpoints);
       return clientInstance.bindedEndpoints;
     },
-    getClient: () :EndpointMap => {
+    getClient: (): EndpointMap => {
       if (!clientInstance) throw Error('Redux http client instance not configured');
       return clientInstance.bindedEndpoints;
     },
