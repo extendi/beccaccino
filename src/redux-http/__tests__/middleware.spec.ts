daa65792c8831600906ec250087ce84b69e00989
sciamp <alessandro.campagni@gmail.com>
2018-07-20 14:19:18 +0200

    Add tests for redux-http/httpClientMiddleware

--- src/redux-http/__tests__/middleware.spec.ts
+++ src/redux-http/__tests__/middleware.spec.ts
@@ -1,4 +1,10 @@
-import { reduxHttpMiddleware }from '@lib/redux-http';
+import {
+  reduxHttpMiddleware,
+  REDUX_HTTP_ACTION_SIGNATURE,
+  REDUX_HTTP_CLIENT_REQUEST,
+  REDUX_HTTP_CLIENT_RESPONSE,
+  REDUX_HTTP_CLIENT_ERROR,
+} from '@lib/redux-http';
 
 const nextSpyMock = jest.fn();
 
@@ -6,16 +12,127 @@ afterEach(() => {
   jest.clearAllMocks();
 });
 
-describe('http Client Middleware', () => {
-  it('Should not handle extraneous actions', () => {
-    const extraneousAction = {
-      type: 'EXTRANEOUS',
+describe('Http Client Middleware', () => {
+  it('does not handle unsigned actions', () => {
+    const action = {
+      type: REDUX_HTTP_CLIENT_REQUEST,
       payload: 'not interesting',
+      execAsync: new Promise((resolve, reject) => resolve('asd')),
     };
 
-    const result = reduxHttpMiddleware(null)(nextSpyMock)(extraneousAction);
+    const result = reduxHttpMiddleware(null)(nextSpyMock)(action);
     expect(result).toBeUndefined();
     expect(nextSpyMock).toHaveReturnedTimes(1);
-    expect(nextSpyMock).toHaveBeenCalledWith(extraneousAction);
+    expect(nextSpyMock).toHaveBeenCalledWith(action);
+  });
+  it('does not handle unrecognised action types', () => {
+    const action = {
+      type: 'NOT_MY_TYPE',
+      payload: 'some stuff here',
+      signature: REDUX_HTTP_ACTION_SIGNATURE,
+      execAsync: new Promise((resolve, reject) => resolve('asd') || reject('foo')),
+    }
+
+    const result = reduxHttpMiddleware(null)(nextSpyMock)(action);
+    expect(result).toBeUndefined();
+    expect(nextSpyMock).toHaveReturnedTimes(1);
+    expect(nextSpyMock).toHaveBeenCalledWith(action);
+  });
+  it('does not handle actions without execAsync', () => {
+    const action = {
+      type: REDUX_HTTP_CLIENT_REQUEST,
+      payload: 'some stuff here',
+      signature: REDUX_HTTP_ACTION_SIGNATURE,
+    }
+
+    const result = reduxHttpMiddleware(null)(nextSpyMock)(action);
+    expect(result).toBeUndefined();
+    expect(nextSpyMock).toHaveReturnedTimes(1);
+    expect(nextSpyMock).toHaveBeenCalledWith(action);
+  });
+  it('does not handle actions without execAsync as Promise', () => {
+    const action = {
+      type: REDUX_HTTP_CLIENT_REQUEST,
+      payload: 'some stuff here',
+      signature: REDUX_HTTP_ACTION_SIGNATURE,
+      execAsync: 'asd asd',
+    }
+
+    const result = reduxHttpMiddleware(null)(nextSpyMock)(action);
+    expect(result).toBeUndefined();
+    expect(nextSpyMock).toHaveReturnedTimes(1);
+    expect(nextSpyMock).toHaveBeenCalledWith(action);
+  });
+  it('dispatches a REDUX_HTTP_CLIENT_REQUEST with binded request details payload', () => {
+    const requestDetails = {
+      urlParmas: {
+        foo: 'bar'
+      },
+      requestPayload: 'some payload here!',
+      endpointName: 'getSomething'
+    }
+    const action = {
+      type: REDUX_HTTP_CLIENT_REQUEST,
+      signature: REDUX_HTTP_ACTION_SIGNATURE,
+      requestDetails,
+      execAsync: new Promise((resolve, reject) => resolve('asd')),
+    };
+
+    reduxHttpMiddleware(null)(nextSpyMock)(action);
+    expect(nextSpyMock).toHaveReturnedTimes(1);
+    expect(nextSpyMock).toHaveBeenCalledWith({
+      type: REDUX_HTTP_CLIENT_REQUEST,
+      requestDetails
+    });
+  });
+  it('dispatches a REDUX_HTTP_CLIENT_RESPONSE when execAsync promise resolves', () => {
+    const requestDetails = {
+      urlParmas: {
+        foo: 'bar'
+      },
+      requestPayload: 'some payload here!',
+      endpointName: 'getSomething'
+    }
+    const action = {
+      type: REDUX_HTTP_CLIENT_REQUEST,
+      signature: REDUX_HTTP_ACTION_SIGNATURE,
+      requestDetails,
+      execAsync: Promise.resolve('the result'),
+    };
+
+    reduxHttpMiddleware(null)(nextSpyMock)(action);
+    action.execAsync.then((result) => {
+      expect(nextSpyMock).toHaveReturnedTimes(2);
+      expect(nextSpyMock).lastCalledWith({
+        type: REDUX_HTTP_CLIENT_RESPONSE,
+        requestDetails,
+        response: result,
+      });
+    });
+  });
+  it('dispatches a REDUX_HTTP_CLIENT_ERROR when execAsync promise fails', () => {
+    const requestDetails = {
+      urlParmas: {
+        foo: 'bar'
+      },
+      requestPayload: 'some payload here!',
+      endpointName: 'getSomething'
+    }
+    const action = {
+      type: REDUX_HTTP_CLIENT_REQUEST,
+      signature: REDUX_HTTP_ACTION_SIGNATURE,
+      requestDetails,
+      execAsync: Promise.reject('the error'),
+    };
+
+    reduxHttpMiddleware(null)(nextSpyMock)(action);
+    action.execAsync.catch((errors) => {
+      expect(nextSpyMock).toHaveReturnedTimes(2);
+      expect(nextSpyMock).lastCalledWith({
+        type: REDUX_HTTP_CLIENT_ERROR,
+        requestDetails,
+        response: errors,
+      });
+    });
   });
 });
--- src/redux-http/middleware.ts
+++ src/redux-http/middleware.ts
@@ -5,16 +5,16 @@ import {
   REDUX_HTTP_CLIENT_ERROR,
   REDUX_HTTP_CLIENT_RESPONSE,
   BindedActionResultPayload,
-}from '@lib/redux-http';
-import { Middleware }from 'redux';
+} from '@lib/redux-http';
+import { Middleware } from 'redux';
 
-const shouldHandleAction = (action: BindedActionPayload) : Boolean => {
+const shouldHandleAction = (action: BindedActionPayload): Boolean => {
   return action.signature === REDUX_HTTP_ACTION_SIGNATURE &&
-  action.type === REDUX_HTTP_CLIENT_REQUEST &&
-  action.execAsync instanceof Promise;
+    action.type === REDUX_HTTP_CLIENT_REQUEST &&
+    action.execAsync instanceof Promise;
 };
 
-const httpClientMiddleware : Middleware = _ => next => (action : BindedActionPayload) => {
+const httpClientMiddleware: Middleware = _ => next => (action: BindedActionPayload) => {
   if (!shouldHandleAction(action)) {
     next(action);
     return;
@@ -23,25 +23,26 @@ const httpClientMiddleware : Middleware = _ => next => (action : BindedActionPay
     type: REDUX_HTTP_CLIENT_REQUEST,
     requestDetails: action.requestDetails,
   });
-  return action.execAsync
-  .then((result) => {
-    const successPayloadToDispatch : BindedActionResultPayload = {
-      type: REDUX_HTTP_CLIENT_RESPONSE,
-      requestDetails: action.requestDetails,
-      response: result,
-    };
-    next(successPayloadToDispatch);
-    Promise.resolve(result);
-  })
-  .catch((errors) => {
-    const errorPayloadToDispatch : BindedActionResultPayload = {
-      type: REDUX_HTTP_CLIENT_ERROR,
-      requestDetails: action.requestDetails,
-      response: errors,
-    };
-    next(errorPayloadToDispatch);
-    Promise.resolve(errors);
-  });
+
+  action.execAsync
+    .then((result) => {
+      const successPayloadToDispatch: BindedActionResultPayload = {
+        type: REDUX_HTTP_CLIENT_RESPONSE,
+        requestDetails: action.requestDetails,
+        response: result,
+      };
+      next(successPayloadToDispatch);
+      Promise.resolve(result);
+    })
+    .catch((errors) => {
+      const errorPayloadToDispatch: BindedActionResultPayload = {
+        type: REDUX_HTTP_CLIENT_ERROR,
+        requestDetails: action.requestDetails,
+        response: errors,
+      };
+      next(errorPayloadToDispatch);
+      Promise.resolve(errors);
+    });
 };
 
 export default httpClientMiddleware;
