3e4c593f2b45ecada1a6a89a342227c893746004
sciamp <alessandro.campagni@gmail.com>
2018-07-20 12:01:27 +0200

    Fix endopoint module impirt in requestHandler

--- src/endpoint/requestHandler.ts
+++ src/endpoint/requestHandler.ts
@@ -1,22 +1,22 @@
-import { AxiosRequestConfig, AxiosInstance }from 'axios';
-import { ErrorTransform, ResponseTransform, EndpointResponse }from '@lib/Endpoint';
+import { AxiosRequestConfig, AxiosInstance } from 'axios';
+import { ErrorTransform, ResponseTransform, EndpointResponse } from '@lib/endpoint';
 
-const defaulTransformFunction = (x : any) : any => x;
-const defaultErrorTransformFunction = (x : any) : any => x;
+const defaulTransformFunction = (x: any): any => x;
+const defaultErrorTransformFunction = (x: any): any => x;
 
 type RequestHandlerParams = {
   requestConfiguration: AxiosRequestConfig,
   errorTransformer: ErrorTransform,
   responseTransformer: ResponseTransform,
-  axiosInstance : AxiosInstance,
+  axiosInstance: AxiosInstance,
 };
 export default function requestHandler({
   requestConfiguration,
   errorTransformer = defaultErrorTransformFunction,
   axiosInstance,
   responseTransformer = defaulTransformFunction,
-}: RequestHandlerParams) : Promise<EndpointResponse> {
+}: RequestHandlerParams): Promise<EndpointResponse> {
   return axiosInstance.request(requestConfiguration)
-  .then(response => ({ rawResponse: response, data: responseTransformer(response.data) }))
-  .catch(error => ({ rawResponse: error, data: errorTransformer(error) }));
+    .then(response => ({ rawResponse: response, data: responseTransformer(response.data) }))
+    .catch(error => ({ rawResponse: error, data: errorTransformer(error) }));
 }
