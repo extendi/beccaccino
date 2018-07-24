c2e6371a379d096cb1c773de00315a528d9c0301
Carmine Di Monaco <dimonaco@extendi.it>
2018-07-24 14:10:06 +0200

    Add test for state selector limit

--- src/redux-http/__tests__/selectors.spec.ts
+++ src/redux-http/__tests__/selectors.spec.ts
@@ -57,6 +57,69 @@ describe('state selectors', () => {
       },
     }]);
   });
+  it('Returns the request and metadata for an existing endpoint with limit of 2', () => {
+    const result = reduxHttpClientSelector({
+      state: {
+        ...baseState,
+        [REDUX_HTTP_CLIENT_REDUCER_NAME]: {
+          ...baseState[REDUX_HTTP_CLIENT_REDUCER_NAME],
+          requests: {
+            ...baseState[REDUX_HTTP_CLIENT_REDUCER_NAME].requests,
+            testEndpoint: [
+              ...baseState[REDUX_HTTP_CLIENT_REDUCER_NAME].requests.testEndpoint,
+              {
+                requestDetails: {
+                  requestId: 'request2',
+                },
+                rawResponse: {},
+                response: { data: ['test2'] },
+              },
+              {
+                requestDetails: {
+                  requestId: 'request3',
+                },
+                rawResponse: {},
+                response: { data: ['test3'] },
+              },
+            ],
+          },
+          requestsMetadata: {
+            request1: {
+              isLoading: false,
+              success: true,
+            },
+            request2: {
+              isLoading: true,
+              success: false,
+            },
+            request3: {
+              isLoading: false,
+              success: true,
+            },
+          },
+        },
+      },
+      endpointName: 'testEndpoint',
+      limit: 2,
+    });
+    expect(result).toEqual([
+      {
+        result: { data: ['test'] },
+        metadata: {
+          isLoading: false,
+          success: true,
+        },
+      },
+      {
+        result: { data: ['test2'] },
+        metadata: {
+          isLoading: true,
+          success: false,
+        },
+      },
+    ]);
+  });
+
   it('Returns the request and undefined metadata for an existing endpoint', () => {
     const result = reduxHttpClientSelector({
       state: baseState,
