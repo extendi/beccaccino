60db9816bdc168f7104b4eaa6291631faece5758
sciamp <alessandro.campagni@gmail.com>
2018-07-26 11:09:33 +0200

    Change library name to "beccaccino"

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
-const libraryName = 'redux-http-client';
+const libraryName = 'beccaccino';
 
 const propTypesExternals = {
   root: 'PropTypes',
