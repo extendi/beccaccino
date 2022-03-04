a4b175e6abb016f171417e488c585d21e42536eb
Leonardo Rosseti <rosseti@extendi.it>
2022-03-04 15:20:26 +0100

    Upgrade package json scripts for builds

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,57 @@
-const path = require('path');
-const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
-const prodConfig = require('./webpack.config.production');
+const path = require("path");
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+const prodConfig = require("./webpack.config.production");
 
-const libraryName = 'beccaccino';
+const libraryName = "beccaccino";
 
 const reduxExternals = {
-  root: 'Redux',
-  commonjs2: 'redux',
-  commonjs: 'redux',
-  amd: 'redux',
+  root: "Redux",
+  commonjs2: "redux",
+  commonjs: "redux",
+  amd: "redux",
 };
 
 const common = {
-  entry: path.join(__dirname, './src/index.ts'),
+  entry: path.join(__dirname, "./src/index.ts"),
   devtool: "source-map",
   resolve: {
     extensions: [".ts", ".tsx", ".js", ".json"],
-    plugins: [new TsconfigPathsPlugin({ configFile: "tsconfig.json" })]
+    plugins: [new TsconfigPathsPlugin({ configFile: "tsconfig.json" })],
   },
   module: {
     rules: [
       { test: /\.tsx?$/, loader: "ts-loader" },
-      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
-    ]
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+    ],
   },
   externals: {
     redux: reduxExternals,
   },
 };
 
-const developmentConfig = [{
-  ...common,
-  output: {
-    path: path.join(__dirname, './dist'),
-    filename: `${libraryName}.js`,
-    library: libraryName,
-    libraryTarget: 'umd',
-    umdNamedDefine: true,
-    globalObject: 'this',
+const developmentConfig = [
+  {
+    ...common,
+    output: {
+      path: path.join(__dirname, "./dist"),
+      filename: `${libraryName}.js`,
+      library: libraryName,
+      libraryTarget: "umd",
+      umdNamedDefine: true,
+      globalObject: "this",
+    },
   },
-},
-{
-  ...common,
-  target: "node",
-  output: {
-    path: path.join(__dirname, './dist'),
-    filename: `${libraryName}-node.js`,
-    library: libraryName,
-    libraryTarget: 'commonjs2',
+  {
+    ...common,
+    target: "node",
+    output: {
+      path: path.join(__dirname, "./dist"),
+      filename: `${libraryName}-node.js`,
+      library: libraryName,
+      libraryTarget: "commonjs2",
+    },
   },
-}];
+];
 
 module.exports = (env) => {
   if (env && env.production) {
